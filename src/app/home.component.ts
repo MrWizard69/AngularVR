0e0a8d8534c9a266d60b87207c5bf4e7ce507e8f
JordanWilson <mrwizard6933@gmail.com>
2017-07-27 00:39:50 -0400

    got the controller to hover over objects and made a wand shape for the controller graphic

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,16 +1,40 @@
 
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef  } from '@angular/core';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+    elem: any;
+    aframe: any;
+    timeout: any;
+
   title = 'Home Page';
 
-    ngOnInit(): void {
+  constructor(ref: ElementRef) {
+        this.elem = ref.nativeElement;
+    }
+
+    ngOnInit(){
         
         console.log("You are on the home page");
+        this.aframe = this.elem.querySelector('a-scene');
+
+        console.log(this.aframe);
+
+        document.querySelector('#hexagon').addEventListener('mouseenter', function () {
+            this.setAttribute('material', 'color', 'blue');
+        });
+        
+        document.querySelector('#hexagon').addEventListener('mouseleave', function () {
+            this.setAttribute('material', 'color', '#EF2D5E');
+        });
+    }
+
+    over(){
+        alert("Worked!");
     }
 }
